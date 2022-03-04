10bd8a108f279836187c1f517ee34a87caf19958
arsil <arsilq@yahoo.com>
2022-03-04 16:11:20 +0700

    Added Fatjar config

--- src/test/java/HexTest.java
+++ src/test/java/HexTest.java
@@ -1,13 +1,11 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class HexTest {
 
     @Test
     void encode() {
         var out = Hex.encode("Hello from HAI MP23");
-        Assertions.assertEquals(out,"48656c6c6f2066726f6d20484149204d503233");
+        Assertions.assertEquals("48656c6c6f2066726f6d20484149204d503233", out);
     }
 }
---
+++ src/test/java/ServerTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+
+class ServerTest {
+
+    @Test
+    void start() throws IOException, InterruptedException {
+        Server.start();
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(URI.create("http://localhost:8888")).build();
+        var response = client.send(request, BodyHandlers.ofString());
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals("Hello from HAI MP23", response.body());
+    }
+}
