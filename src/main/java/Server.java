7af3c2dae7c72e691d7fadf982997b7c0100abc9
arsil <arsilq@yahoo.com>
2022-03-07 21:06:28 +0700

    Added frontend

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,10 +1,14 @@
 import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 
 public class Server {
     public static void start() {
-        var app = Javalin.create().start(8888);
-        app.get("/", ctx -> ctx.result("Hello from HAI MP23"));
-        app.get("/encode/{str}", ctx -> ctx.result(Hex.encode(ctx.pathParam("str"))));
+        var app = Javalin.create(config -> {
+            config.enableCorsForAllOrigins();
+            config.addStaticFiles("/public", Location.CLASSPATH);
+            config.addSinglePageRoot("/", "/public/index.html");
+        }).start(8888);
+        app.get("/api/encode/{str}", ctx -> ctx.result(Hex.encode(ctx.pathParam("str"))));
     }
 
     public static void main(String[] args) {
