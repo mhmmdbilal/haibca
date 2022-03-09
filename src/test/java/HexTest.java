ccbbac541dc70ee37f91c70f52d79496871239f7
Muhammad Bilal <100784968+mhmmdbilal@users.noreply.github.com>
2022-03-09 22:07:16 +0700

    balikin kesemula

--- src/test/java/HexTest.java
+++ src/test/java/HexTest.java
@@ -5,7 +5,7 @@ class HexTest {
 
     @Test
     void encode() {
-        var out = Hex.encode("Hello from HAI MP23ks");
+        var out = Hex.encode("Hello from HAI MP23");
         Assertions.assertEquals("48656c6c6f2066726f6d20484149204d503233", out);
     }
 }
